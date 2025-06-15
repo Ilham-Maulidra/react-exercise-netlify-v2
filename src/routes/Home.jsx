9df4a2c6b1e769f0595d42ea9a99b28c522b7611
Ilham Maulidra <ilhammaulidra@gmail.com>
2025-06-15 23:08:24 +0700

    modified file

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Home = () => {
+  console.log($(".studentName").text(), $(".studentId").text());
   return (
     <>
       <div className="home">
-        <p style={{fontSize: '40px'}}>Keep all your memories here and show others</p>
+        <p style={{ fontSize: "40px" }}>
+          Keep all your memories here and show others
+        </p>
       </div>
     </>
   );
